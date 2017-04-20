d20f5910c7fbc0c65655fda790edd62e24c15729
Luca Soldaini <luca@soldaini.net>
2017-04-20 13:56:48 -0400

    improvements on calling script

--- hon_hash_candidates.js
+++ hon_hash_candidates.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/env node
 
 var hon_md5 =
 {
--- hon_verifier.py
+++ hon_verifier.py
@@ -1,26 +1,43 @@
+import os
 import subprocess
 
 
 class HONVerifier(object):
     def __init__(self,
-                 candgen_path='hon_verifier/hon_hash_candidates.js',
-                 hlist_path='hon_verifier/listeMD5.txt',
+                 candgen_path='hon_hash_candidates.js',
+                 hlist_path='listeMD5.txt',
                  hon_base_url='https://www.hon.ch/HONcode/?'):
-        self.candgen_path = candgen_path
+
         with open(hlist_path) as hlf:
-            hlist = [tuple(line.split()) for line in hlf.read().split('\n')
-                     if len(line.strip()) > 0]
-            self.hashlist = {h: (hon_base_url + p) for h, p in hlist}
+            hlist = [
+                line.split() for line in hlf.read().split('\n')
+                if len(line.strip()) > 0
+            ]
+
+        self.hashlist = {h: (hon_base_url + p) for h, p in hlist}
+
+        # check if the Node.js executable is called "node" or "nodejs"
+        try:
+            subprocess.call(['nodejs'])
+            node_bin_name = 'nodejs'
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                node_bin_name = 'node'
+            else:
+                raise
+
+        self.cmd = [node_bin_name, candgen_path]
 
     def check_url(self, *urls):
         if len(urls) == 0:
             raise RuntimeError('No URL provided.')
 
         proc = subprocess.Popen(
-            [('./' + self.candgen_path)] + list(urls),
+            self.cmd + list(urls),
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
+
         msg_out, msg_err = proc.communicate()
         if len(msg_err.strip()) > 0:
             raise OSError(msg_err.decode('utf-8'))
@@ -33,8 +50,10 @@ class HONVerifier(object):
         results = []
         for cands in cands_group:
 
-            result = [(self.hashlist[cand] if cand in self.hashlist else None)
-                      for cand in cands]
+            result = [
+                (self.hashlist[cand] if cand in self.hashlist else None)
+                for cand in cands
+            ]
             results.append(len(list(filter(lambda x: x, result))) > 0)
 
         if len(results) == 1:
@@ -51,9 +70,9 @@ if __name__ == '__main__':
     except IndexError:
         u1 = 'http://www.webmd.com/anxiety-panic/default.htm'
         u2 = 'https://apple.com'
-        # print(honv.check_url('http://www.webmd.com/anxiety-panic/default.htm'))
-        # print(honv.check_url('http://www.pacemakerclub.com/public/jpage'
-        #                      '/1/p/story/a/storypage/sid/20657/content.do'))
+        print(honv.check_url('http://www.webmd.com/anxiety-panic/default.htm'))
+        print(honv.check_url('http://www.pacemakerclub.com/public/jpage'
+                             '/1/p/story/a/storypage/sid/20657/content.do'))
 
         import time
         IT = 100
